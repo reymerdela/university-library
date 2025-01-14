cb40b294d9f57c3021c843bcc681e1439273402d
reymerdela <reymerdela@users.noreply.github.com>
2025-01-14 15:51:54 -0400

    Add authentication illustration to layout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,6 +13,15 @@ const Layout = ({ children }: { children: ReactNode }) => {
           <div>{children}</div>
         </div>
       </section>
+      <section className="auth-illustration">
+        <Image
+          src="/images/auth-illustration.png"
+          alt="auth illustration"
+          width={1000}
+          height={1000}
+          className="size-full object-cover"
+        />
+      </section>
     </main>
   );
 };
