157c6bfd8bd8c280b6be95d78c820bcf1e266e75
reymerdela <reymerdela@users.noreply.github.com>
2025-01-15 14:53:20 -0400

    Add production API endpoint and resend token to configuration

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,9 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-    databaseUrl: process.env.DATABASE_URL,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
+    databaseUrl: process.env.DATABASE_URL!,
+    resendToken: process.env.RESEND_TOKEN!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLICK_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
