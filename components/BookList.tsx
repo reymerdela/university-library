a747fb2cbc583c176d688549da70b4aed0f57b2e
reymerdela <reymerdela@users.noreply.github.com>
2025-01-15 12:25:55 -0400

    Refactor BookList component to accept title as a prop and add new Avatar component for user profile images

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -4,13 +4,13 @@ import BookCart from "./BookCart";
 interface Props {
   title: string;
   books: Book[];
-  containerClassName: string;
+  containerClassName?: string;
 }
 
-const BookList = ({ books, containerClassName }: Props) => {
+const BookList = ({ books, containerClassName, title }: Props) => {
   return (
     <section className={containerClassName}>
-      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
       <ul className="book-list">
         {books.map((book) => (
           <BookCart key={book.title} {...book} />
