d2dd053770974b170b3de6f179f032f85d0522d2
reymerdela <reymerdela@users.noreply.github.com>
2025-01-18 17:49:09 -0400

    Pass userId prop to BookOverview and adjust BookList to exclude the first book from latestBooks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,12 +13,13 @@ const Home = async () => {
     .from(books)
     .limit(10)
     .orderBy(desc(books.createdAt))) as Book[];
+
   return (
     <>
-      <BookOverview {...latestBooks[0]} />
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
       <BookList
         title="Latest Books"
-        books={latestBooks}
+        books={latestBooks.slice(1)}
         containerClassName="mt-28"
       />
     </>
